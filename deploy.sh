898eff5376017b7af1370d54d7013a22fb6b4174
Jack Firth <jackhfirth@gmail.com>
2016-02-23 20:54:52 -0800

    Re-attempt CI-based deployment of images to Docker Hub

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+set -e
+
+docker login --email="${DOCKER_EMAIL}" --password="${DOCKER_PASSWORD}" --username="${DOCKER_USERNAME}"
+push () { docker push jackfirth/racket:$1; }
+push 6.4
+push 6.3
+push 6.2.1
+push 6.2
+push 6.1.1
+push 6.1
+push 6.0.1
+push 6.0
+push 5.3.6
+push 5.3.5
+push 5.3.4
+push 5.3.3
+push 5.3.2
+push 5.3.1
+push 5.3
+push 5.2.1
+push 5.2
+push 5.1.3
+push 5.1.2
+push 6.4-onbuild
+push 6.4-onbuild-test
+push 6.3-onbuild
+push 6.3-onbuild-test
+push 6.2.1-onbuild
+push 6.2.1-onbuild-test
+push 6.2-onbuild
+push 6.2-onbuild-test
