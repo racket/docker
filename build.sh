b57fcfeca9b8d878cd3c7213b8a635539f2b3b65
Jack Firth <jackhfirth@gmail.com>
2016-02-24 19:19:24 -0800

    Loop over versions to build

--- build.sh
+++ build.sh
@@ -20,30 +20,15 @@ build_onbuild_test_6x () {
   build_template racket-onbuild-test $1-onbuild-test "http://mirror.racket-lang.org/installers/$1/racket-minimal-$1-x86_64-linux-debian-squeeze.sh";
 }
 
-build_6x 6.4
-build_6x 6.3
-build_6x 6.2.1
-build_6x 6.2
-build_6x 6.1.1
-build_6x 6.1
-build_6x 6.0.1
-build_6x 6.0
-build_5x 5.3.6
-build_5x 5.3.5
-build_5x 5.3.4
-build_5x 5.3.3
-build_5x 5.3.2
-build_5x 5.3.1
-build_5x 5.3
-build_5x 5.2.1
-build_5x 5.2
-build_5x 5.1.3
-build_5x 5.1.2
-build_onbuild_6x 6.4
-build_onbuild_6x 6.3
-build_onbuild_6x 6.2.1
-build_onbuild_6x 6.2
-build_onbuild_test_6x 6.4
-build_onbuild_test_6x 6.3
-build_onbuild_test_6x 6.2.1
-build_onbuild_test_6x 6.2
+foreach () {
+  command=$1;
+  shift;
+  for arg in "$@"; do
+    $command $arg;
+  done
+}
+
+foreach build_6x 6.4 6.3 6.2.1 6.2 6.1.1 6.1 6.0.1 6.0
+foreach build_5x 5.3.6 5.3.5 5.3.4 5.3.3 5.3.2 5.3.1 5.3 5.2.1 5.2 5.1.3 5.1.2
+foreach build_onbuild_6x 6.4 6.3 6.2.1 6.2
+foreach build_onbuild_test_6x 6.4 6.3 6.2.1 6.2
