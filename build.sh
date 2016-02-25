e3df124676d37967037a146c285ce730632a2e81
Jack Firth <jackhfirth@gmail.com>
2016-02-24 16:50:21 -0800

    Use template docker file and build arguments instead of duplicated files

--- build.sh
+++ build.sh
@@ -1,25 +1,37 @@
 set -e
 
+build_template () {
+  docker build -t jackfirth/racket:$1 -f racket.Dockerfile --build-arg RACKET_INSTALLER_URL=$2.
+}
+
+build_6x () {
+  build_template $1 "http://mirror.racket-lang.org/installers/$1/racket-minimal-$1-x86_64-linux-debian-squeeze.sh"
+}
+
+build_5x () {
+  build template $1 "http://mirror.racket-lang.org/installers/$1/racket-textual/racket-textual-$1-bin-x86_64-linux-debian-squeeze.sh"
+}
+
 build () { docker build -t jackfirth/racket:$1 $1; }
-build 6.4
-build 6.3
-build 6.2.1
-build 6.2
-build 6.1.1
-build 6.1
-build 6.0.1
-build 6.0
-build 5.3.6
-build 5.3.5
-build 5.3.4
-build 5.3.3
-build 5.3.2
-build 5.3.1
-build 5.3
-build 5.2.1
-build 5.2
-build 5.1.3
-build 5.1.2
+build_6x 6.4
+build_6x 6.3
+build_6x 6.2.1
+build_6x 6.2
+build_6x 6.1.1
+build_6x 6.1
+build_6x 6.0.1
+build_6x 6.0
+build_5x 5.3.6
+build_5x 5.3.5
+build_5x 5.3.4
+build_5x 5.3.3
+build_5x 5.3.2
+build_5x 5.3.1
+build_5x 5.3
+build_5x 5.2.1
+build_5x 5.2
+build_5x 5.1.3
+build_5x 5.1.2
 build 6.4-onbuild
 build 6.4-onbuild-test
 build 6.3-onbuild
--- racket.Dockerfile
+++ racket.Dockerfile
@@ -5,8 +5,7 @@ RUN apt-get update && \
     apt-get install -y wget sqlite3 ca-certificates openssl && \
     rm -rf /var/lib/apt/lists/*
 
-ENV RACKET_VERSION 6.4
-ENV RACKET_INSTALLER_URL http://mirror.racket-lang.org/installers/$RACKET_VERSION/racket-minimal-$RACKET_VERSION-x86_64-linux-debian-squeeze.sh
+ARG RACKET_INSTALLER_URL
 
 RUN wget --output-document=racket-install.sh $RACKET_INSTALLER_URL && \
     echo "yes\n1\n" | /bin/bash racket-install.sh && \
