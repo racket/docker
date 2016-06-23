e6126642579e15520f7e87ef1b010f19656e5905
Jack Firth <jackhfirth@gmail.com>
2016-06-23 00:22:28 -0700

    Fix download link for 6.5

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ build_template () {
 }
 
 build_6x () {
+  build_template racket $1 "http://mirror.racket-lang.org/installers/$1/racket-minimal-$1-x86_64-linux.sh" $1;
+}
+
+build_6x_debian () {
   build_template racket $1 "http://mirror.racket-lang.org/installers/$1/racket-minimal-$1-x86_64-linux-debian-squeeze.sh" $1;
 }
 
@@ -13,10 +17,18 @@ build_5x () {
 }
 
 build_onbuild_6x () {
-  build_template racket-onbuild $1-onbuild "http://mirror.racket-lang.org/installers/$1/racket-minimal-$1-x86_64-linux-debian-squeeze.sh" $1;
+  build_template racket-onbuild $1-onbuild "http://mirror.racket-lang.org/installers/$1/racket-minimal-$1-x86_64-linux.sh" $1;
 }
 
 build_onbuild_test_6x () {
+  build_template racket-onbuild-test $1-onbuild-test "http://mirror.racket-lang.org/installers/$1/racket-minimal-$1-x86_64-linux.sh" $1;
+}
+
+build_onbuild_6x_debian () {
+  build_template racket-onbuild $1-onbuild "http://mirror.racket-lang.org/installers/$1/racket-minimal-$1-x86_64-linux-debian-squeeze.sh" $1;
+}
+
+build_onbuild_test_6x_debian () {
   build_template racket-onbuild-test $1-onbuild-test "http://mirror.racket-lang.org/installers/$1/racket-minimal-$1-x86_64-linux-debian-squeeze.sh" $1;
 }
 
@@ -28,7 +40,10 @@ foreach () {
   done
 }
 
-foreach build_6x 6.5 6.4 6.3 6.2.1 6.2 6.1.1 6.1 6.0.1 6.0
+foreach build_6x 6.5
+foreach build_6x_debian 6.4 6.3 6.2.1 6.2 6.1.1 6.1 6.0.1 6.0
 foreach build_5x 5.3.6 5.3.5 5.3.4 5.3.3 5.3.2 5.3.1 5.3 5.2.1 5.2 5.1.3 5.1.2
-foreach build_onbuild_6x 6.5 6.4 6.3 6.2.1 6.2
-foreach build_onbuild_test_6x 6.5 6.4 6.3 6.2.1 6.2
+foreach build_onbuild_6x 6.5
+foreach build_onbuild_test_6x 6.5
+foreach build_onbuild_6x_debian 6.4 6.3 6.2.1 6.2
+foreach build_onbuild_test_6x_debian 6.4 6.3 6.2.1 6.2
