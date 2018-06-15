b7491d0cd3c954f8b86e3109c7b5f7a4ba1a4217
Jack Firth <jackhfirth@gmail.com>
2018-06-14 20:58:20 -0700

    Remove significants of newlines

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euxfo pipefail
+set -euxfo pipefail;
 
 build_template () {
   declare -r dockerfile="${1}.Dockerfile";
@@ -13,13 +13,13 @@ build_template () {
       --build-arg "${installer_arg}" \
       --build-arg "${version_arg}" \
       .;
-}
+};
 
 installer_url () {
   declare -r version="${1}";
   declare -r installer_path="${2}";
   echo "http://mirror.racket-lang.org/installers/${version}/${installer_path}";
-}
+};
 
 build_6x () {
   declare -r version="${1}";
@@ -31,7 +31,7 @@ build_6x () {
   build_template "racket" "${plain_tag}" "${installer}" "${version}";
   build_template "racket-onbuild" "${onbuild_tag}" "${installer}" "${version}";
   build_template "racket-onbuild-test" "${onbuild_test_tag}" "${installer}" "${version}";
-}
+};
 
 build_6x_debian () {
   declare -r version="${1}";
@@ -43,23 +43,23 @@ build_6x_debian () {
   build_template "racket" "${version}" "${installer}" "${version}";
   build_template "racket-onbuild" "${onbuild_tag}" "${installer}" "${version}";
   build_template "racket-onbuild-test" "${onbuild_test_tag}" "${installer}" "${version}";
-}
+};
 
 build_5x () {
   declare -r version="${1}";
   declare -r installer_path="racket-textual/racket-textual-${version}-bin-x86_64-linux-debian-squeeze.sh";
   declare -r installer="$(installer_url \"${version}\" \"${installer_path}\")" || exit "${?}";
   build_template "racket-old" "${version}" "${installer}" "${version}";
-}
+};
 
 foreach () {
   declare -r command="${1}";
   declare -r args="${@:2}";
   for _arg in "${args}"; do
     "${command}" "${_arg}";
-  done
-}
+  done;
+};
 
-foreach build_6x "6.12" "6.11" "6.10.1" "6.10" "6.9" "6.8" "6.7" "6.6" "6.5"
-foreach build_6x_debian "6.4" "6.3" "6.2.1" "6.2" "6.1.1" "6.1" "6.0.1" "6.0"
-foreach build_5x "5.3.6" "5.3.5" "5.3.4" "5.3.3" "5.3.2" "5.3.1" "5.3" "5.2.1" "5.2" "5.1.3" "5.1.2"
+foreach build_6x "6.12" "6.11" "6.10.1" "6.10" "6.9" "6.8" "6.7" "6.6" "6.5";
+foreach build_6x_debian "6.4" "6.3" "6.2.1" "6.2" "6.1.1" "6.1" "6.0.1" "6.0";
+foreach build_5x "5.3.6" "5.3.5" "5.3.4" "5.3.3" "5.3.2" "5.3.1" "5.3" "5.2.1" "5.2" "5.1.3" "5.1.2";
