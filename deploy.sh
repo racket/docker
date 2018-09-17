114829368cdc86898e901a27748827083b48691e
Paulo Matos <pmatos@linki.tools>
2018-09-17 08:54:33 +0200

    Add support for version 7.0
    7.0 has been released awhile ago. Add support for generating images for 7.0.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -euxfo pipefail;
 
-declare -r LATEST_RACKET_VERSION="6.12";
+declare -r LATEST_RACKET_VERSION="7.0";
 
 docker image tag "jackfirth/racket:${LATEST_RACKET_VERSION}" "jackfirth/racket:latest";
 
@@ -21,6 +21,7 @@ foreach () {
 
 foreach push \
     "latest" \
+    "7.0" \
     "6.12" \
     "6.11" \
     "6.10.1" \
