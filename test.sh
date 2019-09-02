41407fc0f6d6d30ab5cccd7169f6b3b80e797dd7
Bogdan Popa <bogdan@defn.io>
2019-09-02 10:08:13 +0300

    test: use the nevermore package to test images

--- test.sh
+++ test.sh
@@ -6,15 +6,8 @@ source "_common.sh";
 
 test-image () {
   declare -r image="${1}";
-
-  if echo "${image}" | grep "\-full" -q; then
-      test_package="deta-lib";  # rackunit-lib is already installed in the -full variant
-  else
-      test_package="rackunit-lib";
-  fi
-
   docker container run -it "${image}" racket -e "(+ 1 2 3)";
-  docker container run -it "${image}" raco pkg install --auto "${test_package}";
+  docker container run -it "${image}" raco pkg install --auto nevermore;
 };
 
 for image in $(find_images); do
