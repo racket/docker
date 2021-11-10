0a2713603506bce6a274345098291e40b3aae1cc
Bogdan Popa <bogdan@defn.io>
2021-11-10 10:34:04 +0200

    test: run tests non-interactively

--- test.sh
+++ test.sh
@@ -6,12 +6,12 @@ source "_common.sh";
 
 test-image () {
   declare -r image="${1}";
-  docker container run -it "${image}" racket -e "(+ 1 2 3)";
+  docker container run "${image}" racket -e "(+ 1 2 3)";
 
   # The "nevermore" package is an empty package that never changes which we
   # created specifically for this test. This lets us test that package
   # installation works and the default package catalogs are properly configured.
-  docker container run -it "${image}" raco pkg install --auto nevermore;
+  docker container run "${image}" raco pkg install --auto nevermore;
 };
 
 for image in $(find_testable_images "${DOCKER_REPOSITORY}"); do
