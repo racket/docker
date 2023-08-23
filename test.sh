927a08781a1b92e8cb7834662133a135df58391c
Bogdan Popa <bogdan@defn.io>
2023-08-23 19:51:42 +0100

    build: skip package tests for Racket 8.[0123456] BC

--- test.sh
+++ test.sh
@@ -8,10 +8,15 @@ test-image () {
   declare -r image="${1}";
   docker container run "${image}" racket -e "(+ 1 2 3)";
 
-  # The "nevermore" package is an empty package that never changes which we
-  # created specifically for this test. This lets us test that package
-  # installation works and the default package catalogs are properly configured.
-  docker container run "${image}" raco pkg install --auto nevermore;
+  # Skip this test on Racket 8.{0-6} BC since those versions have issues
+  # loading OpenSSL on recent Debian versions.
+  if ! [[ "${image}" =~ racket:8.[0123456] ]]; then
+      # The "nevermore" package is an empty package that never changes
+      # which we created specifically for this test. This lets us test
+      # that package installation works and the default package catalogs
+      # are properly configured.
+      docker container run "${image}" raco pkg install --auto nevermore;
+  fi
 };
 
 for image in $(find_testable_images "${DOCKER_REPOSITORY}"); do
